2ecb2696a38285551e48b4ae34edd519fe3405db
gnostichumor <gnostichumor@gmail.com>
2022-01-21 14:04:30 -0500

    css tweaks and vote ability

--- scripts/1-initialize-sdk.js
+++ scripts/1-initialize-sdk.js
@@ -1,9 +1,12 @@
-import { ThirdwebSDK } from "@3rdweb/sdk";
+// import { ThirdwebSDK } from "@3rdweb/sdk";
+import pkg from '@3rdweb/sdk';
 import ethers from "ethers";
 
 //Importing and configuring our .env file that we use to securely store our environment variables
 import dotenv from "dotenv";
 dotenv.config();
+
+const { ThirdwebSDK } = pkg;
 
 // Some quick checks to make sure our .env is working.
 if (!process.env.PRIVATE_KEY || process.env.PRIVATE_KEY == "") {
